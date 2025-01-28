13493df9ca2644333f1eaf6b06a4ddb5f067b4a3
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-01-28 23:39:18 +0530

    Adding Practice Problems based on level 2 done  on DATE-28/01/2025

---
+++ PracticeProblemsLevel2/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       
+        Question1 question1 = new Question1();
+        question1.FindFactors();
+        Console.ReadKey();  
+
+        
+        Question2 question2 = new Question2();
+        question2.SumOfNaturalNumbers();
+        Console.ReadKey();  
+
+        
+        Question3 question3 = new Question3();
+        question3.CheckLeapYear();
+        Console.ReadKey();   
+
+        
+        Question4 question4 = new Question4();
+        question4.UnitConverter();
+        Console.ReadKey();  
+
+        
+        Question5 question5 = new Question5();
+        question5.UnitConverterExtended();
+        Console.ReadKey();  
+
+        
+        Question6 question6 = new Question6();
+        question6.UnitConverterTemperature();
+        Console.ReadKey();  
+
+        
+        Question7 question7 = new Question7();
+        question7.CheckStudentVotingEligibility();
+        Console.ReadKey();  
+
+        Question8 question8 = new Question8();
+        question8.FindYoungestAndTallest();
+        Console.ReadKey();  
+
+       
+        Question9 question9 = new Question9();
+        question9.CheckNumberProperties();
+        Console.ReadKey();  
+
+        
+        Question10 question10 = new Question10();
+        question10.CalculateBMI();
+        Console.ReadKey();  
+
+      
+        Question11 question11 = new Question11();
+        question11.FindQuadraticRoots();
+        Console.ReadKey();  
+
+       
+        Question12 question12 = new Question12();
+        question12.GenerateRandomNumbers();
+        Console.ReadKey();  
+
+      
+    }
+}
