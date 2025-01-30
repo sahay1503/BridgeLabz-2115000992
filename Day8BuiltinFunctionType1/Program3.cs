106ded15c25ec0fd46b71c305a665f114d04c2aa
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-01-30 23:14:41 +0530

    Adding PracticeProblems of Type1 solved on DATE-30/01/2025

---
+++ Day8BuiltinFunctionType1/Program3.cs
@@ -0,0 +1,11 @@
+using System;
+public class Program3 { 
+    public static void FormatCurrentDate()
+    {
+        Console.WriteLine("Current Date in different formats:");
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine($"dd/MM/yyyy: {currentDate:dd/MM/yyyy}");
+        Console.WriteLine($"yyyy-MM-dd: {currentDate:yyyy-MM-dd}");
+        Console.WriteLine($"EEE, MMM dd, yyyy: {currentDate:ddd, MMM dd, yyyy}");
+    }
+}
