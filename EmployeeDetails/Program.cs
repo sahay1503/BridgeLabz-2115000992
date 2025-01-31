0f1e6627a4ae106c006a5431069f2575d74284ed
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-01-31 20:37:12 +0530

    Adding EmployeeDetails Project on DATE-31/01/2025

---
+++ EmployeeDetails/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create Employee object for JAmit Yadav
+        Employee emp1 = new Employee("Amit Yadav", 101, 55000);
+        // Create Employee object for Nishant Chauhan
+        Employee emp2 = new Employee("Nishant Chauhan", 102, 62000);
+
+        // Display employee details
+        Console.WriteLine("----Employee 1----");
+        emp1.DisplayDetails();
+        Console.WriteLine("\n----Employee 2----");
+        emp2.DisplayDetails();
+    }
+}
