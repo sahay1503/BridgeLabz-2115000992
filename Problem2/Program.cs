b22343bc7758d35a9d4fe94193241b7b2a76eef9
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-02-15 21:55:42 +0530

    [Siddhant] Add : Adding some problems based on StringBuilder on DATE-15/02/2025

---
+++ Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string result = RemoveDuplicates(input);
+        Console.WriteLine("String after removing duplicates: " + result);
+    }
+
+    static string RemoveDuplicates(string str)
+    {
+        HashSet<char> seen = new HashSet<char>();
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in str)
+        {
+            if (!seen.Contains(c))
+            {
+                seen.Add(c);
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
