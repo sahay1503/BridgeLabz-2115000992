13db8042a00ffb85f38d9fb01e380dbba2ad609f
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-02-24 16:09:22 +0530

    [Siddhant] Add : Adding probles based on Exception Handling solved on 21/02/2025

---
+++ Problem-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "info.txt";
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string firstLine = reader.ReadLine();
+                Console.WriteLine("First Line: " + firstLine);
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Error reading file");
+        }
+    }
+}
