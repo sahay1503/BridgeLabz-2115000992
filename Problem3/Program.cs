e39b3d54974fc2c92ef7f0d6858e3ec13d7729bc
Siddhant <113369476+sahay1503@users.noreply.github.com>
2025-02-25 11:11:51 +0530

    [Siddhant] Add : Adding problems based on C# Reflection solved on 24/02/2025

---
+++ Problem3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+class Calculator
+{
+    private int Multiply(int a, int b) => a * b; // Private method
+}
+
+class ReflectionPrivateMethodExample
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+        Type type = typeof(Calculator);
+
+        // Access private method
+        MethodInfo method = type.GetMethod("Multiply", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (method != null)
+        {
+            // Invoke the private method
+            int result = (int)method.Invoke(calc, new object[] { 5, 10 });
+            Console.WriteLine("Multiplication Result: " + result);
+        }
+        else
+        {
+            Console.WriteLine("Method not found!");
+        }
+    }
+}
---
+++ Problem7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+class Configuration
+{
+    private static string API_KEY = "OriginalAPIKey";
+
+    public static void ShowApiKey()
+    {
+        Console.WriteLine("Current API Key: " + API_KEY);
+    }
+}
+
+class ReflectionStaticFieldExample
+{
+    static void Main()
+    {
+        Type type = typeof(Configuration);
+
+        // Access private static field
+        FieldInfo field = type.GetField("API_KEY", BindingFlags.NonPublic | BindingFlags.Static);
+
+        if (field != null)
+        {
+            // Display original value
+            Configuration.ShowApiKey();
+
+            // Modify the static field value
+            field.SetValue(null, "NewSecureAPIKey");
+
+            // Display modified value
+            Configuration.ShowApiKey();
+        }
+        else
+        {
+            Console.WriteLine("Field not found!");
+        }
+    }
+}
